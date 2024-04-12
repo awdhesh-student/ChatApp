426a41dc30d8e0df06d7beb9ab07531f9410d142
awdhesh-prasad <prasadawdheshp145@gmail.com>
2024-04-12 14:21:15 +0530

    updated all urls

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
    useEffect(() => {
       if (authUser) {
-         const socket = io("http://localhost:8000", {
+         const socket = io("https://chatapp-a5v9.onrender.com/", {
             query: {
                userId: authUser._id
             }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // your backend server address
+        target: "https://chatapp-a5v9.onrender.com/", // your backend server address
       },
     },
   },
